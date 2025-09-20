8b693ad552565c980d1867713b0fe3ac98bf6125
Charles Barilleaux <chbarr@bandman.local>
2025-09-20 10:54:09 -0400

    Initial Variable experimentation

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # The Basics
 
 Working through learning Swift, much as I had my attempt at Rust. This is starting with [The Basics](https://docs.swift.org/swift-book/documentation/the-swift-programming-language/thebasics) section of the documentation (though it may spill beyond that). 
+
+* **2025-09-20**
+    * Did some variable assignment and printing
+    
--- Sources/TheBasics.swift
+++ Sources/TheBasics.swift
@@ -3,3 +3,21 @@
 // Swift programming language.
 
 print("Hello, Charles and Beso!") //Beso was on my desk as I typed this. 
+
+var Name="Beso"
+print("Hello, \(Name)!")
+Name="Charles"
+print("and hello, \(Name)!\nDo standard escape codes work?")
+print("2+2=\(2+2)")
+print("\n----------")
+//Cool multi-line string assignment
+let biys="""
+    Blue canary in the outlet by the nightstand
+    Who watches over you?
+    Make a little birdhouse in your soul
+        Not to put too fine a point on it
+        Say your the only bee in my bonnet
+    Make a little birdhouse in your soul
+    """
+print(biys)
+print("----------")
