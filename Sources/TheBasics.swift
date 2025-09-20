f774248466b62976e52d07163a8020aa644a7e10
Charles Barilleaux <chbarr@bandman.local>
2025-09-20 10:43:42 -0400

    Did some minor setup stuff.

---
+++ Sources/TheBasics.swift
@@ -0,0 +1,5 @@
+// TheBasics
+// This is a sandbox for working through the Swift documentation, an attempt to learn the
+// Swift programming language.
+
+print("Hello, Charles and Beso!") //Beso was on my desk as I typed this. 
